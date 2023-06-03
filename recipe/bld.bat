f0aca33f9c7556ed30ec1c3d38c4594c2a87693c
Daniel Brown <daniel.d.brown@adelaide.edu.au>
2023-06-03 21:30:50 +0100

    trying custom build scripts

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,18 +1,9 @@
 :: remove old egg-info directory
 rmdir /s /q src\finesse.egg-info
 
-:: tell python about the compiler
-set CFGDIR="%PREFIX%\Lib\distutils"
-set CFG=%CFGDIR%\distutils.cfg
-if not exist "%CFGDIR%\" mkdir "%CFGDIR\"
+:: build the package
+%PYTHON% setup.py build_ext --inplace --compiler=msvc
 if errorlevel 1 exit 1
-echo [config] > "%CFG%"
-if errorlevel 1 exit 1
-echo compiler=msvc >> "%CFG%"
-echo [build] >> "%CFG%"
-echo compiler=msvc >> "%CFG%"
-echo [build_ext] >> "%CFG%"
-echo compiler=msvc >> "%CFG%"
 
 :: build the package
 %PYTHON% -m pip install . -vv
