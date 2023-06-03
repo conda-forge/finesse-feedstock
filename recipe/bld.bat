58d369cce1f0bceb9146d0db966fe2364dea02b7
Daniel Brown <daniel.d.brown@adelaide.edu.au>
2023-06-03 23:10:53 +0100

    don't force cythonize on second pip install

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -2,9 +2,11 @@
 rmdir /s /q src\finesse.egg-info
 
 :: build the package
+set CYTHON_FORCE=1
 %PYTHON% setup.py build_ext --inplace --compiler=msvc
 if errorlevel 1 exit 1
 
 :: build the package
+set CYTHON_FORCE=0
 %PYTHON% -m pip install . -vv
 if errorlevel 1 exit 1
